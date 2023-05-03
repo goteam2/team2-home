ea1d051c31fec878c234e2760a91cfb238328b80
goteam2 <info@goteam2.io>
2023-05-03 17:47:37 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],  theme: {
+    fontFamily: {
+      sans: ['nimbus-sans-extended', 'neue-haas-grotesk-text', 'sans-serif'],
+    },
+    
+    extend: {},
+  },
+  plugins: [],
+}
+
